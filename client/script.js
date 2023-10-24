edbab1ae40d338d3b8673df9a274753bcbeea6f0
jishimw <114191929+jishimw@users.noreply.github.com>
2023-10-24 10:01:42 -0400

    Suggested Step 7: item 2 front-end functionality
    Signed-off-by: jishimw <114191929+jishimw@users.noreply.github.com>

--- client/script.js
+++ client/script.js
@@ -18,3 +18,28 @@ document.getElementById('getSuperheroInfo').addEventListener('click', () => {
             superheroInfoDiv.innerHTML = 'Superhero not found';
         });
 });
+
+document.getElementById('getSuperheroPowers').addEventListener('click', () => {
+    const superheroIdPowers = document.getElementById('superheroIdPowers').value;
+    console.log(`Requested superhero powers for ID: ${superheroIdPowers}`); // Add this line for debugging
+    fetch(`/api/superhero/${superheroIdPowers}/powers`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Error: ${response.status} - ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            const superheroPowersDiv = document.getElementById('superheroPowers');
+            if (data.error) {
+                superheroPowersDiv.innerHTML = data.error;
+            } else {
+                superheroPowersDiv.innerHTML = JSON.stringify(data, null, 2);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+            const superheroPowersDiv = document.getElementById('superheroPowers');
+            superheroPowersDiv.innerHTML = 'An error occurred.';
+        });
+});
